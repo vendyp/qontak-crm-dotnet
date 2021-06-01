2d5b6bcf7301fb4003669a90f87d41e5a73232f1
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2021-06-02 02:31:53 +0700

    Add property field type constant

---
+++ src/Qontak.Crm/Constants/PropertyFieldTypeConstant.cs
@@ -0,0 +1,19 @@
+namespace Qontak.Crm.Constants
+{
+    public static class PropertyFieldTypeConstant
+    {
+        public static string SingleLineText = "Single-line text";
+        public static string DropdownSelect = "Dropdown select";
+        public static string Number = "Number";
+        public static string Date = "Date";
+        public static string Upload = "Upload";
+        public static string MultipleSelect = "Multiple select";
+        public static string Percentage = "Percentage";
+        public static string TextArea = "Text Area";
+        public static string Checklist = "Checklist";
+        public static string Photo = "Photo";
+        public static string URL = "URL";
+        public static string Signature = "Signature";
+        public static string Gps = "gps";
+    }
+}
